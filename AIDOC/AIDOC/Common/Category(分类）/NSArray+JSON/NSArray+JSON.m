d681ddbec01461ff995fac018fbc6e52a507068c
polaris_qun <polaris_qun@163.com>
2018-08-14 18:58:48 +0800

    NSArray+JSON

---
+++ AIDOC/AIDOC/Common/Category(分类）/NSArray+JSON/NSArray+JSON.m
@@ -0,0 +1,41 @@
+#import "NSArray+JSON.h"
+
+@implementation NSArray (JSON)
+
+- (NSString *)toJSONString {
+    NSData *data = [NSJSONSerialization dataWithJSONObject:self
+                                                   options:NSJSONReadingMutableLeaves | NSJSONReadingAllowFragments
+                                                     error:nil];
+    
+    if (data == nil) {
+        return nil;
+    }
+    
+    NSString *string = [[NSString alloc] initWithData:data
+                                             encoding:NSUTF8StringEncoding];
+    return string;
+}
+
+- (NSString *)toReadableJSONString {
+    NSData *data = [NSJSONSerialization dataWithJSONObject:self
+                                                   options:NSJSONWritingPrettyPrinted
+                                                     error:nil];
+    
+    if (data == nil) {
+        return nil;
+    }
+    
+    NSString *string = [[NSString alloc] initWithData:data
+                                             encoding:NSUTF8StringEncoding];
+    return string;
+}
+
+- (NSData *)toJSONData {
+    NSData *data = [NSJSONSerialization dataWithJSONObject:self
+                                                   options:NSJSONWritingPrettyPrinted
+                                                     error:nil];
+    
+    return data;
+}
+
+@end
